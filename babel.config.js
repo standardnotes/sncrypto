2d8eccead8190ee9b1e1c3007e5b9bed4ea1dcda
Baptiste Grob <60621355+baptiste-grob@users.noreply.github.com>
2020-08-06 16:37:46 +0200

    fix: update babel config to target async/await/wasm browsers

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,8 @@
 module.exports = function (api) {
-  api.cache(true);
-
-  const presets = [
-    '@babel/typescript',
-    ["@babel/preset-env", {
-      "targets": {
-        "chrome": "58",
-        "ie": "11"
-      }
-    }]
-  ];
-
-  const plugins = [
-    ['@babel/plugin-transform-runtime', {
-      "useESModules": true,
-      "regenerator": true,
-      "absoluteRuntime": false,
-      "corejs": false,
-      "helpers": false,
-    }],
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-syntax-dynamic-import"
-  ];
+  api.cache.forever();
 
   return {
-    presets,
-    plugins
+    presets: ["@babel/preset-env"],
+    plugins: ["@babel/plugin-syntax-dynamic-import"],
   };
 };
