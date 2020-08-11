4a080efeb646dbf9ca3dffdfcfa9d081b4dc6de0
Baptiste Grob <60621355+baptiste-grob@users.noreply.github.com>
2020-08-11 19:23:01 +0200

    fix: typescript declarations

--- dist/@types/web/index.js
+++
@@ -1,2 +0,0 @@
-export { SNWebCrypto } from './crypto';
-export { isWebCryptoAvailable, Buffer, stringToArrayBuffer, arrayBufferToString, arrayBufferToHexString, hexStringToArrayBuffer, base64ToArrayBuffer, arrayBufferToBase64, hexToBase64, base64ToHex, base64Encode, base64Decode, toBuffer, } from './utils';
---
+++ lib/sncrypto-web.ts
@@ -0,0 +1,16 @@
+export { SNWebCrypto } from './web/crypto';
+export {
+  isWebCryptoAvailable,
+  Buffer,
+  stringToArrayBuffer,
+  arrayBufferToString,
+  arrayBufferToHexString,
+  hexStringToArrayBuffer,
+  base64ToArrayBuffer,
+  arrayBufferToBase64,
+  hexToBase64,
+  base64ToHex,
+  base64Encode,
+  base64Decode,
+  toBuffer,
+} from './web/utils';
